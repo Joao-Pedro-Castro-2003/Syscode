ffb8dc57ba50d36f5ac566f8a11f5fa8db2ecf8f
Joao-Pedro-Castro-2003 <jp.castro0509@gmail.com>
2024-08-18 11:21:12 -0300

    retirando duplicidade de arquivos

--- WebApp/CreateTable.sql
+++
@@ -1,8 +0,0 @@
-create table population (
-	Entity varchar(50),
-    Code Varchar(100),
-    Year int,
-    Population varchar(100),
-	);
-
-	select * from population
--- WebApp/Proc.sql
+++
@@ -1,9 +0,0 @@
-create PROCEDURE BuscaPopulacaoPorPais
-    @Nome_Pais VARCHAR(100)
-AS
-BEGIN
-    SELECT Population, Year
-    FROM population
-    WHERE Entity like @Nome_Pais
-	order by Year
-END
